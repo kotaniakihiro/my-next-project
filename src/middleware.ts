d14cffc169a2205ae0130e7181708a2eae860151
akihirock <akihiroetegami@gmail.com>
2025-02-18 10:44:38 +0900

    seven12

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-import { NextRequest, NextResponse } from "next/server";
 import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
 
 export const middleware = createNextAuthMiddleware();
