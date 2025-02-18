1322027002b24509ac3dfbf8d571e1ecaf6ba733
akihirock <akihiroetegami@gmail.com>
2025-02-18 10:32:52 +0900

    seven9

---
+++ middleware.ts
@@ -0,0 +1,8 @@
+import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
+
+export const middleware = createNextAuthMiddleware();
+
+// すべてのページに適用
+export const config = {
+  matcher: ["/(.*)"],
+};
